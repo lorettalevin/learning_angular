c75a0084dc12a113fb4b02c6e38f2ff84e7babe3
lorettalevin <lorettalevin4@gmail.com>
2018-08-04 21:17:56 +0200

    practicing 2-way data-binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,24 +8,35 @@ import { Component, OnInit } from '@angular/core';
   //     <app-server></app-server>`,
   styles: ['./servers.component.css']
 })
+
 export class ServersComponent implements OnInit {
-    allowNewServer = false;
-    serverCreationStatus = 'No server was created!'
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  userName = ''
 
   constructor() {
-      setTimeout(() => {
-          this.allowNewServer = true;
-      }, 2000);
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
   ngOnInit() {
   }
 
   onCreateServer() {
-      this.serverCreationStatus = 'Server was created!'
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName
   }
 
-  onUpdateServerName(event: any) {
-      console.log("EVENTTTT", event)
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onUpdateUsername(event: Event) {
+    this.userName = (<HTMLInputElement>event.target).value;
+  }
+
+  onResetUsername() {
+    this.newUsername = "Username has been reset to: " + this.userName
   }
 }
